f415b7ce08d69beb28a1f78df38caa764e49de76
Swamy <swamyboyak@email.com>
2026-04-21 04:20:53 +0530

    updated app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 # ================= DATABASE CONFIG =================
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///workerconnect.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///workerconnect_new.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
